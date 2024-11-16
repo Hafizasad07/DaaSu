5cd522767813ca6ab20e108d12d826702b09006e
Hafizasad07 <hafizasadrafique@gmail.com>
2024-11-16 17:58:31 +0500

    updated

--- assets/contact/contact.php
+++ assets/contact/contact.php
@@ -5,14 +5,15 @@
    $mail = new PHPMailer;
    $mail->isSMTP();
    $mail->SMTPDebug = 2;
+//    for demo 
    $mail->Host = 'smtp.gmail.com';
    $mail->Port = 587;
    $mail->SMTPAuth = true;
-   $mail->Username = 'saadideveloper516@gmail.com';
-   $mail->Password = 'ebzonbcrbsqnduva';
+   $mail->Username = 'daasuapp@gmail.com';  // Replace with your email
+   $mail->Password = '61f38b028705c4';  // Replace with your Gmail app password
    extract($_POST);
-   $mail->setFrom('saadideveloper516@gmail.com', $name);
-   $mail->addAddress('saadideveloper516@gmail.com', 'DaaSu App'); 
+   $mail->setFrom('daasuapp@gmail.com', $name);
+   $mail->addAddress('daasuapp@gmail.com', 'DaaSu App'); 
    $mail->isHTML(true);   
    $mail->Body = "<h2> Contact Form </h2> <b>Name: </b>" .$name . " <b>Email: </b>" .$email . "<br><b>Company Name: </b>" . $company_name . "<br><b>Message: </b>" . $message;
    if (!$mail->send()) {
