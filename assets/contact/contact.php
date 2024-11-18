ce6e3b377ce37462a543b72f3429697c9a3327c5
Hafizasad07 <hafizasadrafique@gmail.com>
2024-11-18 12:50:19 +0500

    update

--- assets/contact/contact.php
+++ assets/contact/contact.php
@@ -10,12 +10,12 @@
    $mail->Port = 587;
    $mail->SMTPAuth = true;
    $mail->Username = 'daasuapp@gmail.com';  // Replace with your email
-   $mail->Password = '61f38b028705c4';  // Replace with your Gmail app password
+   $mail->Password = 'ykusezwpfvawdvaw';  // Replace with your Gmail app password
    extract($_POST);
    $mail->setFrom('daasuapp@gmail.com', $name);
    $mail->addAddress('daasuapp@gmail.com', 'DaaSu App'); 
    $mail->isHTML(true);   
-   $mail->Body = "<h2> Contact Form </h2> <b>Name: </b>" .$name . " <b>Email: </b>" .$email . "<br><b>Company Name: </b>" . $company_name . "<br><b>Message: </b>" . $message;
+   $mail->Body = "<h2> Contact Form </h2> <b>Name: </b>" .$name . "<br> <b>Email: </b>" .$email . "<br><b>Company Name: </b>" . $company_name . "<br><b>Message: </b>" . $message;
    if (!$mail->send()) {
        echo 'Mailer Error: ' . $mail->ErrorInfo;
    } else {
